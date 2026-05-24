098f2e0bf7075a20c262dfdb50c1c5b2b5a0121d
Ted <tedlanham@gmail.com>
2026-05-24 03:14:08 -0400

    chore: update down_revision in migration script for consistency

--- api/alembic/versions/a2b3c4d5e6f7_remove_stop_type.py
+++ api/alembic/versions/a2b3c4d5e6f7_remove_stop_type.py
@@ -9,7 +9,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = 'a2b3c4d5e6f7'
-down_revision: Union[str, None] = 'f6a7b8c9d0e1'
+down_revision: Union[str, None] = 'e6f7a8b9c0d1'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
